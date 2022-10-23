36d8ffb359ab70de212842960ecdce1a19f03808
theRealBenForce <TheRealBenForce@gmail.com>
2022-10-22 18:45:21 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # mister-hassio
-Script that can be run on a MiSTer FPGA that sends current game data to Home Assistant.
+This script that can be run on a MiSTer FPGA that sends current game data to Home Assistant.
+Feel free to leave suggestions on better ways of interacting with the two platforms.
+
+This script is in early development and I have a few plans that will be breaking changes, but it is currently working.
+
+This works by first registering itself as a startup script in MiSTer (thanks wizzo), and then polling on a regular interval, configured at 60 seconds by default.
+
+![Home Assitant dashboard card](/images/dashboard.png)
+
+# Setup
+## Home Assistant
+* Create a long lived token for API authorizations and add this to the hassio.ini file.
+* Create a Helper of "text" type for your game. Default ini settings are mister_game.
+* Create a Helper of "text" type for your platform. Default ini settings are mister_platform.
+* Add the api integration.
+
+To quickly do all these things, add the below to you configuration.yaml
+```
+# Integrations
+api:
+
+# Helpers
+input_text:
+  mister_game:
+    name: Mister Game
+    mode: text
+    max: 255
+    icon: mdi:controller-classic
+  mister_platform:
+    name: Mister Platform
+    mode: text
+    max: 255
+    icon: mdi:television-classic
+```
+
+## MiSTer
+* Set recents=1 in mister.ini.
+* Update the hassio.ini file as needed. You will at lest need to add your long lived token.
+* Place hassio.ini and hassio.sh in your /Scripts dir.
+
+# Rendering in Home Assistant
+Please share examples of how you are using this. I added some YAML to the samples folder to get you started.
+
+# Future Updates
+Some of my plans to improve off this proof of concept:
+* Combine both entities into a single entity with metadata attributes
+* Integrate with a 3rd party API to send game art and metadata.
+* Change from polling to file watching. Maybe. 
+* Improve game and platform name formatting.
+* Improve logging
---
+++ hassio.sh
@@ -0,0 +1,198 @@
+#!/usr/bin/env python
+import configparser
+import glob
+import json
+import logging
+import os
+import sys
+import time
+import urllib3
+
+INI_PATH = "/media/fat/Scripts/hassio.ini"
+if os.path.exists(INI_PATH):
+    ini = configparser.ConfigParser()
+    ini.read(INI_PATH)
+    HA_HOSTNAME = ini["HA_INFO"]["HA_HOSTNAME"]
+    HA_PROTOCOL = ini["HA_INFO"]["HA_PROTOCOL"]
+    HA_PORT = ini["HA_INFO"]["HA_PORT"]
+    HA_GAME_ENTITY = ini["HA_INFO"]["HA_GAME_ENTITY"]
+    HA_PLATFORM_ENTITY = ini["HA_INFO"]["HA_PLATFORM_ENTITY"]
+    HA_LONG_LIVED_TOKEN = ini["HA_INFO"]["HA_LONG_LIVED_TOKEN"]
+    LOG_LEVEL = ini["GENERAL"]["LOG_LEVEL"]
+    POLL_TIME = ini["GENERAL"]["POLL_TIME"]
+
+file_handler = logging.FileHandler(filename='/tmp/hassio.log', encoding='utf-8')
+stdout_handler = logging.StreamHandler(stream=sys.stdout)
+handlers = [stdout_handler]
+#handlers.append(file_handler)
+numeric_level = getattr(logging, LOG_LEVEL.upper(), 10)
+logging.basicConfig(
+    level=numeric_level, 
+    format='[%(asctime)s] {%(filename)s:%(lineno)d} %(levelname)s - %(message)s',
+    handlers=handlers
+)
+logger = logging.getLogger('hassio_logger')
+
+
+SAM_PATH = "/tmp/SAM_Game.txt"
+CORES_RECENT_PATH = "/media/fat/config/cores_recent.cfg"
+BASEURL = "{}://{}:{}/api/".format(HA_PROTOCOL, HA_HOSTNAME, HA_PORT)
+STARTUP_SCRIPT = "/media/fat/linux/user-startup.sh"
+
+#cmd = 'while inotifywait -e close_write /tmp/SAM_Game.txt; do /media/fat/Scripts/hassio.sh; done'
+#os.system(cmd)
+
+
+# run a refresh on each boot, thanks wizzo
+def try_add_to_startup():
+    logger.info('Adding script to startup.')
+    if not os.path.exists(STARTUP_SCRIPT):
+        return
+    with open(STARTUP_SCRIPT, "r") as f:
+        if "Startup Hass Watcher" in f.read():
+            return
+    with open(STARTUP_SCRIPT, "a") as f:
+        f.write(
+            "\n# Startup Hass Watcher\n[[ -e /media/fat/Scripts/hassio.sh ]] && /media/fat/Scripts/hassio.sh refresh\n"
+        )
+    return
+
+
+def find_most_recent():
+    list_of_files = glob.glob('/media/fat/config/*_recent_*.cfg') # * means all if need specific format then *.csv
+    if os.path.isfile(SAM_PATH):
+        logger.debug('Determined SAM is active on system.')
+        list_of_files.append(SAM_PATH)
+    latest_file = max(list_of_files, key=os.path.getctime)
+    if latest_file == SAM_PATH:
+        logger.debug('Determined SAM is most recent launch.')
+        game, platform = get_game_from_sam()
+    else:
+        logger.debug('Determined RECENTS is most recent launch.')
+        game, platform = get_game_from_cores_recents(latest_file)
+    platform = get_friendly_platform_name(platform)
+    logger.debug("Latest file: {}".format(latest_file))
+    logger.debug("Latest game: {}".format(game))
+    logger.debug("Latest platform: {}".format(platform))
+    return game, platform
+
+
+def get_game_from_sam():
+    logger.debug('Getting recent launched game from SAM')
+    with open(SAM_PATH) as f:
+        line = f.readline()
+    f.close
+    arr = line.split('(')
+    game = arr[0].strip()
+    platform = arr[-1].split(')')[0]
+    return game, platform
+
+
+def get_game_from_cores_recents(filepath: str):
+    logger.debug('Getting recent launched game from cores_recents')
+    with open(filepath) as f:
+        game_path = f.readline().split('\x00')[0]
+    f.close
+    platform = game_path.split("/")[1]
+    game = game_path.split("/")[2].split(".")[0]
+    return game, platform
+
+
+def get_friendly_platform_name(platform: str):
+    logger.debug('Converting {} to a friendly name.'.format(platform))
+    if platform.lower() == "arcade":
+        return "Arcade"
+    elif platform.lower() == "sms":
+        return "Sega Master System"
+    elif platform.lower() == "genesis":
+        return "Sega Genesis"
+    elif platform.lower() == "nes":
+        return "Nintendo Entertainment System"
+    elif platform.lower() == "snes":
+        return "Super Nintendo Entertainment System"
+    else:
+        return platform
+
+def test_api():
+    try:
+        logger.debug('Testing Home Assistant API.')
+        http = urllib3.PoolManager()
+        r = http.request(
+            'GET', 
+            BASEURL,
+            headers={
+                "authorization": 'Bearer ' + HA_LONG_LIVED_TOKEN,
+                "content-type": 'application/json'
+            }
+        )
+        if r.status == 200:
+            logger.debug('Home Assistant API is alive.')
+        elif r.status == 401:
+            logger.error('Home Assistant returned status code {}. You are unauthorized. Check your long lived token.'.format(str(r.status)))
+        elif r.status == 403:
+            logger.error('Home Assistant returned status code {}. You are forbidden. Check your long lived token and ip_bans.yaml.'.format(str(r.status)))
+        else:
+            logger.error('Home Assistant test api failed and returned status code {}'.format(str(r.status)))
+        return r.status
+    except Exception:
+        logger.error(str(Exception))
+        return None
+
+def get_entity(entity):
+    try:
+        logger.debug('Checking for {} entity'.format(entity))
+        http = urllib3.PoolManager()
+        r = http.request(
+            'GET',
+            "{}states/{}".format(BASEURL, entity),
+            headers={
+                "authorization": 'Bearer ' + HA_LONG_LIVED_TOKEN,
+                "content-type": 'application/json'
+            }
+        )
+        if r.status == 200:
+            logger.debug("{} entity was found.".format(entity))
+            logger.debug(json.loads(r.data.decode('utf-8'))['state'])
+            return json.loads(r.data.decode('utf-8'))
+        elif r.status == 404:
+            logger.debug("{} entity can't be found. Check readme for requirements.".format(entity))
+        else:
+            logger.debug("An unknown error has occurred.".format(entity))
+    except Exception:
+        logger.error(str(Exception))
+    return
+
+
+def update_entity(entity: str, state: str, attributes: dict):
+    http = urllib3.PoolManager()
+    data = {
+        'state': state,
+        'attributes': attributes
+        }
+    encoded_data = json.dumps(data).encode('utf-8')
+    r = http.request(
+        'POST', 
+        "{}states/{}".format(BASEURL, entity),
+        body=encoded_data,
+        headers= {
+            "authorization": 'Bearer ' + HA_LONG_LIVED_TOKEN,
+            "content-type": 'application/json'
+        }
+    )
+    return
+
+
+try_add_to_startup()
+while True:
+    if test_api() == 200:
+        current_hass_state_game = get_entity(HA_GAME_ENTITY)
+        current_hass_state_platform = get_entity(HA_PLATFORM_ENTITY)
+        if current_hass_state_game and current_hass_state_platform: 
+            game, platform = find_most_recent()
+            logger.info("Latest: {} - {}".format(platform, game))
+            latest_game = game
+            latest_platform = platform
+            update_entity(HA_GAME_ENTITY, game, current_hass_state_game['attributes'])
+            update_entity(HA_PLATFORM_ENTITY, platform, current_hass_state_platform['attributes'])
+    print("Sleeping {} seconds...".format(POLL_TIME))
+    time.sleep(int(POLL_TIME))
